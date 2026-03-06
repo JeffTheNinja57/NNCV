334f8c32f683e2792722f6876678f0a911800729
JeffTheNinja57 <mihny.angheluta@gmail.com>
2026-03-06 17:31:07 +0100

    update download script to use HF_TOKEN for data download

--- final_assignment/download_docker_and_data.sh
+++ final_assignment/download_docker_and_data.sh
@@ -4,12 +4,13 @@
 #SBATCH --cpus-per-task=9
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_mig
-#SBATCH --time=3:00:00
+#SBATCH --time=6:00:00
 
 # Pull container from dockerhub
 apptainer pull container.sif docker://cclaess/5lsm0:v1
 
 # Use the huggingface-cli package inside the container to download the data
 mkdir -p data
+HF_TOKEN=$(grep '^HF_TOKEN' .env | cut -d= -f2)
 apptainer exec container.sif \
-    huggingface-cli download TimJaspersTue/5LSM0 --local-dir ./data --repo-type dataset
+    huggingface-cli download TimJaspersTue/5LSM0 --local-dir ./data --repo-type dataset --token "$HF_TOKEN"
